cfc59f4170456516c551887e7edf58315f4b8748
Ethan Bellem <ethan.bellem@ocsbstudent.ca>
2018-05-22 12:27:21 -0400

    Initial commit

---
+++ main.lua
@@ -0,0 +1,12 @@
+-----------------------------------------------------------------------------------------
+--
+-- main.lua
+--
+--Created by Ethan Bellem
+--Created on May 2018
+-----------------------------------------------------------------------------------------
+local composer = require("composer")
+
+display.setStatusBar(display.HiddenStutusBar)
+
+composer.gotoScene( "scene1" )
